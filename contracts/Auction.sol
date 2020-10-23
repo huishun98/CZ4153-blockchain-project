748dbb71c374f4d03a4e918adaaf562faef7ca05
juztinfied <z_d_143@hotmail.com>
2020-10-23 10:47:00 +0800

    price change linearly

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -3,21 +3,24 @@ pragma solidity ^0.6.2;
 import "./HuiToken.sol";
 
 contract Auction {
-    //hello
     HuiToken public huiToken;
     address public owner;
     address public wallet;
     uint256 public weiRaised;
 
     uint256 public openingRate;
-    // uint256 public currentRate; // may not need current rate due to calcCurrentTokenPrice()
     uint256 public reserveRate;
-    // uint256 public closingRate; // may not need current rate due to calcCurrentTokenPrice()
+    uint256 public closingRate; 
 
-    uint256 public totalPotMinTokens; // may not need, can be computed. (currently calculation is in frontend)
+    uint256 public totalPotMinTokens;
+    uint256 public totalSupply; 
+    uint public tokensLeft;
 
     uint256 public openingTime;
     uint256 public closingTime;
+
+    mapping(address => uint) public totalBidAmt;
+    mapping(address => bool) public isBidding;
 
     Stages public stage;
 
@@ -53,17 +56,36 @@ contract Auction {
         _;
     }
 
-    mapping(address => uint) public totalBidAmt;
-    mapping(address => bool) public IsBidding;
+    modifier callerIsBidding() {
+        if (!(isBidding[msg.sender]))
+            // Only owner is allowed to proceed
+            revert();
+        _;
+    }
+
+
 
     event BidStaked(address beneficiary, uint256 amount);
 
-    constructor(HuiToken _huiToken, uint256 _rate) public {
+    constructor(HuiToken _huiToken, uint256 _openingRate, uint256 _reserveRate) public {
         huiToken = _huiToken;
-        owner = msg.sender;
-        openingRate = _rate;
+        owner = msg.sender; //owner is the EOA that created this contract 
+        wallet = address(this);
+        // huiToken.transferFrom(huiToken.getTokenOwner(), wallet, huiToken.balanceOf(owner)); //transfer all tokens to this contract 
+        // huiToken.transfer(owner, 1);
+        openingRate = _openingRate;
+        reserveRate = _reserveRate;
+
         totalPotMinTokens = 0;
-        // TODO - ADD RESERVE RATE
+        totalSupply = huiToken.totalSupply();
+    }
+
+    function getBalance() public view returns (uint256) {
+        return wallet.balance;
+    }
+
+    function withdraw() public isOwner atStage(Stages.AuctionEnded){
+        msg.sender.transfer(wallet.balance);
     }
 
     function calcCurrentTokenPrice() public returns (uint256) {
@@ -71,14 +93,11 @@ contract Auction {
             return openingRate;
         } 
         if (stage == Stages.AuctionStarted || stage == Stages.AuctionEnded) {
-            // TODO - MATH TO CALCULATE CURRENT TOKEN PRICE
-            uint256 currentRate = openingRate;
-            return currentRate;
+            return ((openingRate-reserveRate)*(now-openingTime)/(20 minutes) + openingRate);
         }
     }
 
-
-    function randomFunciont () public onlyWhileOpen {
+    function randomFunction () public onlyWhileOpen {
 
     }
 
@@ -95,40 +114,37 @@ contract Auction {
         stage = Stages.AuctionEnded;
     }
 
-    function stakeBid() public payable atStage(Stages.AuctionStarted) {
+    function stakeBid(uint256 amount) public payable atStage(Stages.AuctionStarted) {
         require(msg.sender != address(0));
         require(msg.value > 0, "amount cannot be 0 or less");
-
-        // TODO - TAKE NOTE OF VALUE STAKED BY BIDDER
+        require(msg.value == amount, "amount indicated does not match msg.value!");
+        require(((weiRaised+msg.value)/calcCurrentTokenPrice()) <= totalSupply, "demand exceeds supply!");
 
         weiRaised += msg.value;
         totalBidAmt[msg.sender] += msg.value; // TODO - CHECK IF VALUE ADDED IS CORRECT
+        tokensLeft = totalSupply - (weiRaised/calcCurrentTokenPrice());
 
-        // TODO - CHECK THAT NUMBER OF TOKENS LEFT IS ENOUGH
-        // TODO - STORE ETHER IN WALLET IF VALID
-
-        // if(IsBidding[msg.sender] == true) {
-        //     totalBidAmt[msg.sender] += msg.value;
-        // }
-        // else {
-        //     IsBidding[msg.sender] = true;
-        //     totalBidAmt[msg.sender] = msg.value;
-        // }
+        if(isBidding[msg.sender] == true) {
+            totalBidAmt[msg.sender] += msg.value;
+        }
+        else {
+            isBidding[msg.sender] = true;
+            totalBidAmt[msg.sender] = msg.value;
+        }
 
         // emit BidStaked(_beneficiary, msg.value);
     }
 
     // GETTERS
-    function getBid() public view returns (uint256) {
+    function getBid() public view callerIsBidding returns (uint256) {
         return totalBidAmt[msg.sender];
     }
 
     // TODO - ALLOW USER TO CLAIM TOKENS
-    function claimTokens() public { // atStage(Stages.AuctionEnded)
-        uint bidAmt = totalBidAmt[msg.sender]; 
-        require (bidAmt > 0, "No more tokens for you!"); // user has either collected or has not bid.
+    function claimTokens() public callerIsBidding { //atStage(Stages.AuctionEnded)
+        uint userBidAmt = totalBidAmt[msg.sender]; 
         totalBidAmt[msg.sender] = 0;
-        uint tokensOwed = bidAmt/calcCurrentTokenPrice(); 
+        uint tokensOwed = userBidAmt/calcCurrentTokenPrice(); 
         huiToken.transfer(msg.sender, tokensOwed); // TODO - BUG - VM Exception while processing transaction: revert ERC20: transfer amount exceeds balance
     }
 }
--- contracts/HuiToken.sol
+++ contracts/HuiToken.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.6.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HuiToken is ERC20 {
-    address private _owner;
+    address private owner;
 
     constructor() public ERC20("HuiToken", "Hui") {
         _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
-
+        owner = msg.sender;
     }
 
     function burnAllRemaining() external {
-        require(msg.sender == _owner, "burner is not owner of the this token");
+        require(msg.sender == owner, "burner is not owner of the this token");
         _burn(msg.sender, 0);
+    }
+
+    function getTokenOwner() public view returns (address){ 
+        return owner;
     }
 
 }
